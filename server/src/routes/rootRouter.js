5b9d739692207962a01799919f9eabbba0cde3cf
Solomon Montagno <sjmontagno@gmail.com>
2023-05-26 16:19:55 -0400

    update import for recipe router

--- server/src/routes/rootRouter.js
+++ server/src/routes/rootRouter.js
@@ -4,13 +4,13 @@ import usersRouter from "./api/v1/usersRouter.js";
 import clientRouter from "./clientRouter.js";
 import plantsRouter from "./api/v1/plantRouter.js";
 import weatherRouter from "./api/v1/weatherRouter.js";
-import recipeRouter from "./api/v1/recipesRouter.js";
+import recipesRouter from "./api/v1/recipesRouter.js";
 const rootRouter = new express.Router();
 
 rootRouter.use("/api/v1/user-sessions", userSessionsRouter);
 rootRouter.use("/api/v1/users", usersRouter);
 rootRouter.use("/api/v1/plants", plantsRouter)
-rootRouter.use("/api/v1/recipes", recipeRouter)
+rootRouter.use("/api/v1/recipes", recipesRouter)
 rootRouter.use("/api/v1/weather", weatherRouter)
 rootRouter.use("/", clientRouter);
 //place your server-side routes here
